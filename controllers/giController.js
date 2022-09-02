b0a74afb9a39f7ccb6fdb8be143f565fad178697
Sanghak Kim <k3hppk@gmail.com>
2022-09-02 09:38:42 +0900

    Basic CRUD all done

--- controllers/giController.js
+++ controllers/giController.js
@@ -225,11 +225,119 @@ exports.gi_delete_post = (req, res, next) => {
 };
 
 // GET request to update Gi
-exports.gi_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Gi update GET");
+exports.gi_update_get = (req, res, next) => {
+  async.parallel(
+    {
+      gi(callback) {
+        Gi.findById(req.params.id).exec(callback);
+      },
+      brands(callback) {
+        // What does brand need....
+        Brand.find({}, "name").exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.gi === null && results.brand_list === null) {
+        const err = new Error("Information not found");
+        err.status = 404;
+        return next(err);
+      }
+
+      res.render("gi_form", {
+        title: "Update Gi",
+        brand_list: results.brands,
+        gi: results.gi,
+      });
+    }
+  );
 };
 
 // POST request to update Gi
-exports.gi_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Gi update POST");
-};
+exports.gi_update_post = [
+  body("name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Name must be specified")
+    .isLength({ max: 30 })
+    .withMessage("Max 30 characters")
+    .matches(/^[a-z0-9 ]+$/i)
+    .withMessage("Only alphanumeric value is allowed for Name"),
+  body("description")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Description must be specified")
+    .isLength({ max: 50 })
+    .withMessage("Max 50 characters")
+    .matches(/^[a-z0-9 ]+$/i)
+    .withMessage("Only alphanumeric value is allowed for Description"),
+  body("brand", "Brand must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("size", "Size must not be empty.").trim().isLength({ min: 1 }).escape(),
+  body("price")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("price must be specified")
+    .isNumeric()
+    .withMessage("Only numeric value is allowed for price")
+    .isInt({ min: 0 })
+    .withMessage("Price shouldn't be below than 0"),
+  body("number_in_stock")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("price must be specified")
+    .isNumeric()
+    .withMessage("Only numeric value is allowed for price")
+    .isInt({ min: 0 })
+    .withMessage("Number in stock shouldn't be below than 0"),
+
+  (req, res, next) => {
+    // get information about errors from the request
+    const errors = validationResult(req);
+
+    // create a new gi to save in the db
+    const gi = new Gi({
+      _id: req.params.id,
+      name: req.body.name,
+      description: req.body.description,
+      brand: req.body.brand,
+      size: req.body.size,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock,
+    });
+
+    // if errors - re-render the update view with errors
+    if (!errors.isEmpty()) {
+      Brand.find({}, "name").exec(function (err, brands) {
+        if (err) {
+          return next(err);
+        }
+
+        // NOTE: Here, you need to pass the information that user has filled out
+        res.render("gi_form", {
+          title: "Update Gi",
+          brand_list: brands,
+          errors: errors.array(),
+          gi: gi,
+        });
+      });
+    } else {
+      Gi.findByIdAndUpdate(req.params.id, gi, {}, function (err, gi) {
+        if (err) {
+          return next(err);
+        }
+
+        res.redirect(gi.url);
+      });
+    }
+    // update the gi
+  },
+];
